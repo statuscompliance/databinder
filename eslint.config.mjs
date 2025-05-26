62cd5bad2a92688d229104b0bb408c96a815e030
[[alvarobernal2412]] <alvarobc2412@gmail.com>
2025-05-27 00:38:51 +0200

    chore: add config files for code style

---
+++ eslint.config.mjs
@@ -0,0 +1,28 @@
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import stylisticJs from '@stylistic/eslint-plugin';
+
+export default [
+  {
+    plugins: {
+      '@stylistic/js': stylisticJs,
+    },
+    rules: {
+      'no-empty': ['error', { allowEmptyCatch: true }],
+      '@stylistic/js/quotes': ['error', 'single', { avoidEscape: true }],
+      '@stylistic/js/linebreak-style': ['error', 'unix'],
+      '@stylistic/js/eol-last': ['error', 'always'],
+      '@stylistic/js/indent': ['error', 2],
+      'unicode-bom': ['error', 'never'],
+    },
+    languageOptions: {
+      sourceType: 'module', // Change this to 'module'
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+        process: 'readonly',
+      },
+    },
+  },
+  pluginJs.configs.recommended,
+];
